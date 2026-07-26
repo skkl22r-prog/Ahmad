960cad542d46d5f3651b2bd6768be66be24242e2
skkl22r-prog <skkl22r@gmail.com>
2026-07-26 18:16:07 +0300

    Update countdown target date to December 13, 2026

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useLang } from "@/i18n/LanguageContext";
 
-const TARGET = new Date("2026-07-31T19:30:00+03:00").getTime();
+const TARGET = new Date("2026-12-13T19:30:00+03:00").getTime();
 
 const Countdown = () => {
 const { t: translate, lang } = useLang();

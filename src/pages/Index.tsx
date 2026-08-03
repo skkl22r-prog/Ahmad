2b7b2d9b9ead5a692a454336ae72809e95af7c94
skkl22r-prog <skkl22r@gmail.com>
2026-08-03 17:22:24 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { MapPin, Heart, QrCode, Baby, Camera, Clock } from "lucide-react";
-import invitationImg from "@/assets/video-output-83F7863D-297C-4BAA-B4A1-A9C9A89D3632-1.mp4";
+import invitationImg from "@/assets/video-output-D9F9194A-28DF-4021-A146-402C093BEF8F-1.mp4";
 import Envelope from "@/components/Envelope";
 import SprayParticles from "@/components/SprayParticles";
 import Reveal from "@/components/Reveal";
